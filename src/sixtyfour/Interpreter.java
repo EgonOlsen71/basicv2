e048cb9aefc329823f7fc114c5ad7178c5810732
EgonOlsen <info@jpct.net>
2016-07-05 19:52:05 +0200

    Import fixed

--- src/sixtyfour/Interpreter.java
+++ src/sixtyfour/Interpreter.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import sixtyfour.elements.commands.Command;
-import sixtyfour.elements.commands.Data;
 import sixtyfour.elements.commands.Rem;
 import sixtyfour.parser.Line;
 import sixtyfour.parser.Parser;
