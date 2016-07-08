fcad5943d236feab81089ee57f29ff857c0102bd
EgonOlsen <info@jpct.net>
2016-07-08 21:13:41 +0200

    More compatible now

--- src/sixtyfour/elements/systemvars/Time.java
+++ src/sixtyfour/elements/systemvars/Time.java
@@ -1,39 +1,55 @@
-/*
- * 
- */
 package sixtyfour.elements.systemvars;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import sixtyfour.elements.Variable;
 import sixtyfour.system.Machine;
 
+
 /**
  * The Class Time.
+ * 
+ * @author EgonOlsen
  */
-public class Time extends Variable {
-	
-	/** The start. */
-	private long start = System.currentTimeMillis();
-
-	/**
+public class Time
+  extends Variable
+{
+  
+  /**
 	 * Instantiates a new time.
 	 */
-	public Time() {
-		super("TI", 0);
-	}
+  public Time()
+  {
+    super("TI", 0);
+  }
 
-	/* (non-Javadoc)
-	 * @see sixtyfour.elements.Variable#getValue()
-	 */
-	@Override
-	public Object getValue() {
-		return (int) ((float) (System.currentTimeMillis() - start) / (1000f / 60f));
-	}
 
-	/* (non-Javadoc)
-	 * @see sixtyfour.elements.Variable#eval(sixtyfour.system.Machine)
-	 */
-	@Override
-	public Object eval(Machine machine) {
-		return getValue();
-	}
+  /* (non-Javadoc)
+   * @see sixtyfour.elements.Variable#getValue()
+   */
+  @Override
+  public Object getValue()
+  {
+    return null;
+  }
+
+
+  /* (non-Javadoc)
+   * @see sixtyfour.elements.Variable#eval(sixtyfour.system.Machine)
+   */
+  @Override
+  public Object eval(Machine machine)
+  {
+    TimeDate tid = (TimeDate) machine.getVariable("TI$");
+    Date td = tid.getIntervalValue();
+    Calendar cally = Calendar.getInstance();
+    cally.setTime(td);
+    int h = cally.get(Calendar.HOUR_OF_DAY);
+    int m = cally.get(Calendar.MINUTE);
+    int s = cally.get(Calendar.SECOND);
+    int ss = cally.get(Calendar.MILLISECOND);
+    return (int) ((ss + 1000 * s + 1000 * 60 * m + 1000 * 60 * 60 * h) / (1000f / 60f));
+  }
+
 }
