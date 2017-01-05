c13eb65f68aea925a3af4debd952c5353120603f
EgonOlsen <info@jpct.net>
2017-01-05 17:44:07 +0100

    Empty strings fixed

--- src/main/java/com/sixtyfour/basicshell/ShellInputProvider.java
+++ src/main/java/com/sixtyfour/basicshell/ShellInputProvider.java
@@ -2,36 +2,27 @@ package com.sixtyfour.basicshell;
 
 import com.sixtyfour.plugins.InputProvider;
 
-
 /**
  * @Author nietoperz809
  */
-public class ShellInputProvider
-  implements InputProvider
-{
-  private BasicShell shellFrame;
+public class ShellInputProvider implements InputProvider {
+	private BasicShell shellFrame;
 
+	public ShellInputProvider(BasicShell shellFrame) {
+		this.shellFrame = shellFrame;
+	}
 
-  public ShellInputProvider(BasicShell shellFrame)
-  {
-    this.shellFrame = shellFrame;
-  }
+	@Override
+	public Character readKey() {
+		if (shellFrame.peek() && shellFrame.getString().length() > 0)
+			return shellFrame.getString().charAt(0);
+		return null;
+	}
 
-
-  @Override
-  public Character readKey()
-  {
-    if (shellFrame.peek())
-      return shellFrame.getString().charAt(0);
-    return null;
-  }
-
-
-  @Override
-  public String readString()
-  {
-    String s = shellFrame.getString();
-    s = s.substring(s.indexOf('?') + 1);
-    return s;
-  }
+	@Override
+	public String readString() {
+		String s = shellFrame.getString();
+		s = s.substring(s.indexOf('?') + 1);
+		return s;
+	}
 }
