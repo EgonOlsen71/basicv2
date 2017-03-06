6ba5b015d7faf9f4e23c4568ef0dd038f77c8013
EgonOlsen <info@jpct.net>
2017-03-06 21:05:49 +0100

    Additional ROM routine example

--- src/test/java/com/sixtyfour/test/RomTest.java
+++ src/test/java/com/sixtyfour/test/RomTest.java
@@ -1,27 +1,37 @@
 package com.sixtyfour.test;
 
+import com.sixtyfour.Assembler;
 import com.sixtyfour.Basic;
 import com.sixtyfour.Loader;
 import com.sixtyfour.plugins.impl.RamSystemCallListener;
+import com.sixtyfour.system.Machine;
 
 /**
  * @author EgonOlsen
- *
+ * 
  */
-public class RomTest
-{
-  public static void main(String[] args)
-  {
-    testRomAccess();
-  }
+public class RomTest {
+	public static void main(String[] args) {
+		testRomAccess();
+		testRomCalc();
+	}
 
+	private static void testRomAccess() {
+		String[] vary = Loader.loadProgram("src/test/resources/rom/romtest.bas");
+		Basic inty = new Basic(vary);
+		inty.getMachine().addRoms();
+		inty.setSystemCallListener(new RamSystemCallListener(inty.getMachine()));
+		inty.run();
+	}
 
-  private static void testRomAccess()
-  {
-    String[] vary = Loader.loadProgram("src/test/resources/rom/romtest.bas");
-    Basic inty = new Basic(vary);
-    inty.getMachine().addRoms();
-    inty.setSystemCallListener(new RamSystemCallListener(inty.getMachine()));
-    inty.run();
-  }
+	private static void testRomCalc() {
+		String[] code = Loader.loadProgram("src/test/resources/rom/math.asm");
+		Assembler asm = new Assembler(code);
+		asm.compile();
+		Machine machine = asm.getMachine();
+		machine.addRoms();
+		asm.run();
+		System.out.println(machine.getCpu().convertFloat(8192));
+		System.out.println(machine.getCpu().convertFloat(0x61) + " ~ " + Math.sqrt(machine.getCpu().convertFloat(8192)));
+	}
 }
---
+++ src/test/resources/rom/math.asm
@@ -0,0 +1,12 @@
+*=$c000
+		lda #2
+		ldy #36
+		jsr 45969
+		ldx #00
+		ldy #$20
+		jsr 48087
+		lda #0
+		ldy #$20
+		jsr 48034
+		jsr 49009
+		rts
