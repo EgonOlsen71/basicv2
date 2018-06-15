8cabf51680c9a5d25e9b5ed3dbaf6886af744ad3
Foerster-H <helge.foerster@schluetersche.de>
2018-06-15 15:16:33 +0200

    Tweaked GOSUB in native runtime

--- src/test/java/com/sixtyfour/test/TransformerTest.java
+++ src/test/java/com/sixtyfour/test/TransformerTest.java
@@ -44,7 +44,7 @@ public class TransformerTest {
 		// testTransformer2();
 		// testTransformer4();
 		// testTransformer5();
-//		 testTransformerFractal();
+		 testTransformerFractal();
 		// testTransformer6();
 		// testTransformer7();
 		// testTransformerPrime();
@@ -66,7 +66,7 @@ public class TransformerTest {
 		// testTransformer22();
 		// testTransformer23();
 		// testTransformerFrog();
-//		 testTransformerAffine();
+		 testTransformerAffine();
 		// testTransformer24();
 		// testTransformer25();
 		// testTransformer26();
