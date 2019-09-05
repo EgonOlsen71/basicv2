af296a7cda55cf8589214f0d3ba34e584f3c0122
EgonOlsen <info@jpct.net>
2019-09-05 19:05:23 +0200

    Improved error checking

---
+++ src/main/java/com/sixtyfour/util/CompilerException.java
@@ -0,0 +1,14 @@
+package com.sixtyfour.util;
+
+/**
+ * @author EgonOlsen
+ *
+ */
+public class CompilerException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//
+}
