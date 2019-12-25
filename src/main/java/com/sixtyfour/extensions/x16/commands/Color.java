f7ec3b4f4625bcffa41733f276d6adac653e3db2
EgonOlsen <info@jpct.net>
2019-12-25 22:54:42 +0100

    Init

---
+++ src/main/java/com/sixtyfour/extensions/x16/commands/Color.java
@@ -0,0 +1,91 @@
+package com.sixtyfour.extensions.x16.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sixtyfour.Logger;
+import com.sixtyfour.cbmnative.NativeCompiler;
+import com.sixtyfour.config.CompilerConfig;
+import com.sixtyfour.elements.Type;
+import com.sixtyfour.elements.commands.AbstractCommand;
+import com.sixtyfour.parser.Atom;
+import com.sixtyfour.parser.Parser;
+import com.sixtyfour.parser.cbmnative.CodeContainer;
+import com.sixtyfour.system.BasicProgramCounter;
+import com.sixtyfour.system.Machine;
+
+/**
+ * The COLOR command.
+ */
+public class Color extends AbstractCommand {
+
+	/** The pars. */
+	private List<Atom> pars;
+
+	/**
+	 * Instantiates a new wait.
+	 */
+	public Color() {
+		super("COLOR");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sixtyfour.elements.commands.AbstractCommand#parse(java.lang.String, int,
+	 * int, int, boolean, sixtyfour.system.Machine)
+	 */
+	@Override
+	public String parse(CompilerConfig config, String linePart, int lineCnt, int lineNumber, int linePos,
+			boolean lastPos, Machine machine) {
+		super.parse(config, linePart, lineCnt, lineNumber, linePos, lastPos, machine);
+		term = Parser.getTerm(config, this, linePart, machine, true);
+		pars = Parser.getParameters(term);
+
+		if (pars.size() < 1 || pars.size() > 2) {
+			syntaxError(linePart);
+		}
+
+		checkTypes(pars, linePart, Type.STRING, Type.STRING);
+		return null;
+	}
+
+	@Override
+	public List<CodeContainer> evalToCode(CompilerConfig config, Machine machine) {
+		Atom fg = pars.get(0);
+		NativeCompiler compiler = NativeCompiler.getCompiler();
+		List<String> before = new ArrayList<String>();
+		List<String> after = new ArrayList<String>();
+		List<String> expr = new ArrayList<String>();
+
+		expr.addAll(compiler.compileToPseudoCode(config, machine, fg));
+
+		if (pars.size() == 2) {
+			// BG Color given
+			expr.addAll(compiler.compileToPseudoCode(config, machine, pars.get(1)));
+			after.add("POP Y");
+			after.add("POP X");
+			after.add("JSR TEXTBGCOLOR");
+		} else {
+			after.add("POP X");
+			after.add("JSR TEXTCOLOR");
+		}
+
+		CodeContainer cc = new CodeContainer(before, expr, after);
+		List<CodeContainer> ccs = new ArrayList<CodeContainer>();
+		ccs.add(cc);
+		return ccs;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sixtyfour.elements.commands.AbstractCommand#execute(sixtyfour.system.
+	 * Machine)
+	 */
+	@Override
+	public BasicProgramCounter execute(CompilerConfig config, Machine machine) {
+		Logger.log("Call to COLOR ignored by local runtime!");
+		return null;
+	}
+}
---
+++ src/main/java/com/sixtyfour/extensions/x16/functions/Joy.java
@@ -0,0 +1,80 @@
+package com.sixtyfour.extensions.x16.functions;
+
+import java.util.List;
+
+import com.sixtyfour.elements.Type;
+import com.sixtyfour.elements.functions.AbstractFunction;
+import com.sixtyfour.system.Machine;
+
+/**
+ * The RND function.
+ */
+public class Joy extends AbstractFunction {
+
+	/**
+	 * Instantiates a new rnd.
+	 */
+	public Joy() {
+		super("JOY");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sixtyfour.parser.Atom#getType()
+	 */
+	@Override
+	public Type getType() {
+		return Type.REAL;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sixtyfour.parser.Atom#eval(sixtyfour.system.Machine)
+	 */
+	@Override
+	public Object eval(Machine machine) {
+		// This doesn't query anything and simply returns 0
+		return 0f;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sixtyfour.elements.functions.AbstractFunction#isDeterministic()
+	 */
+	@Override
+	public boolean isDeterministic() {
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sixtyfour.elements.functions.AbstractFunction#toCode(com.sixtyfour
+	 * .system.Machine)
+	 */
+	@Override
+	public String toCode(Machine machine) {
+		return "0";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sixtyfour.elements.functions.AbstractFunction#getParameterCount()
+	 */
+	public int getParameterCount() {
+		return 1;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.sixtyfour.elements.functions.AbstractFunction#addNativeFunctionCall(java.util.List)
+	 */
+	@Override
+	public boolean addNativeFunctionCall(List<String> code) {
+		code.add("JSR JOY");
+		return true;
+	}
+}
