8da547b63b90819ba47d0b60c21d4a5734c30300
EgonOlsen <info@jpct.net>
2020-09-25 08:21:19 +0200

    Improved compressor

--- src/main/java/com/sixtyfour/util/Compressor.java
+++ src/main/java/com/sixtyfour/util/Compressor.java
@@ -25,6 +25,12 @@ import com.sixtyfour.Loader;
  */
 public class Compressor {
 
+	public static boolean FAST = true;
+	
+	private static final int MAX_WINDOW_SIZE = 128;
+	private static final int MIN_WINDOW_SIZE = 12;
+	private static final int CHUNK_SIZE = 32768;
+
 	public static void main(String[] args) throws Exception {
 		testCompressor("C:\\Users\\EgonOlsen\\Desktop\\test.txt");
 		testCompressor("C:\\Users\\EgonOlsen\\Desktop\\++affine.prg");
@@ -43,14 +49,16 @@ public class Compressor {
 
 	public static byte[] compress(byte[] dump) {
 		long time = System.currentTimeMillis();
-		int windowSize = 128;
-		int minSize = 12;
+		int windowSize = MAX_WINDOW_SIZE;
+		int minSize = MIN_WINDOW_SIZE;
 		int len = dump.length;
-		int windowPos = 0;
+		if (len > 65536) {
+			throw new RuntimeException("This compressor can't compress anything larger than 64KB!");
+		}
 		byte[] window = new byte[windowSize];
-		fillWindow(dump, window, windowPos);
+		fillWindow(dump, window, 0);
 
-		List<Part> parts = findMatches(dump, windowSize, minSize, len, windowPos, window);
+		List<Part> parts = findMatches(dump, windowSize, minSize, len, window, FAST);
 
 		byte[] bos = compress(parts, dump);
 
@@ -100,6 +108,7 @@ public class Compressor {
 			} while (target > 0);
 		}
 		if (dataPos < clen) {
+			System.out.println(clen + "/" + dataPos + "/" + pos + "/" + res.length + "/" + (clen - dataPos));
 			System.arraycopy(bytes, dataPos, res, pos, clen - dataPos);
 			pos += (bytes.length - dataPos);
 		}
@@ -170,58 +179,73 @@ public class Compressor {
 	 * @param len
 	 * @param windowPos
 	 * @param window
+	 * @param fastMode Less compression, but a lot faster.
 	 * @return
 	 */
-	private static List<Part> findMatches(byte[] dump, int windowSize, int minSize, int len, int windowPos,
-			byte[] window) {
+	private static List<Part> findMatches(byte[] dump, int windowSize, int minSize, int len, byte[] window,
+			boolean fastMode) {
 		int curSize = windowSize;
 		int largest = 0;
+		int windowPos = 0;
 		List<Part> parts = new ArrayList<>();
 		byte[] covered = new byte[len];
 
-		do {
-			for (int i = windowPos + curSize; i < len - curSize; i++) {
-				if (covered[i] > 0) {
-					continue;
-				}
-				boolean match = true;
+		int cnt = 0;
+		int chunkSize = CHUNK_SIZE;
 
-				for (int p = 0; p < curSize; p++) {
-					if (covered[i + p] != 0) {
-						match = false;
-						break;
+		for (int lenPart = Math.min(len, chunkSize); lenPart <= len; lenPart += chunkSize) {
+			do {
+				for (int i = windowPos + curSize; i < lenPart - curSize; i++) {
+					if (covered[i] > 0) {
+						continue;
 					}
-					if (dump[i + p] != window[p]) {
-						match = false;
-						if (p > largest) {
-							largest = p;
+					cnt++;
+					boolean match = true;
+
+					for (int p = 0; p < curSize; p++) {
+						if (covered[i + p] != 0) {
+							match = false;
+							break;
 						}
-						break;
+						if (dump[i + p] != window[p]) {
+							match = false;
+							if (p > largest) {
+								largest = p;
+							}
+							break;
+						}
+					}
+
+					if (match) {
+
+						for (int h = i; h < i + curSize; h++) {
+							covered[h] = 1;
+						}
+
+						Part part = new Part(windowPos, i, curSize);
+						parts.add(part);
+						i += curSize - 1;
 					}
 				}
-
-				if (match) {
-					
-					for (int h = i; h < i + curSize; h++) {
-						covered[h] = 1;
+				if (largest >= minSize) {
+					curSize = largest;
+					largest = 0;
+				} else {
+					if (fastMode) {
+						windowPos += Math.max(largest, 1);
+						if (windowPos + windowSize >= lenPart) {
+							windowPos -= (windowPos + windowSize - lenPart);
+						}
+					} else {
+						windowPos++;
 					}
-					
-					Part part = new Part(windowPos, i, curSize);
-					parts.add(part);
-					i += curSize - 1;
+					curSize = windowSize;
+					largest = 0;
+					fillWindow(dump, window, windowPos);
 				}
-			}
-			if (largest >= minSize) {
-				curSize = largest;
-				largest = 0;
-			} else {
-				windowPos++;
-				curSize = windowSize;
-				largest = 0;
-				fillWindow(dump, window, windowPos);
-			}
-		} while (windowPos + curSize < len);
-
+			} while (windowPos + curSize < lenPart);
+			windowPos = lenPart - curSize;
+		}
 		Collections.sort(parts, new Comparator<Part>() {
 
 			@Override
@@ -232,6 +256,7 @@ public class Compressor {
 		});
 
 		// parts.forEach(p -> System.out.println(p));
+		log("Iterations taken: " + cnt);
 
 		return parts;
 	}
