39cb3231add4b2c0a270cff640ee055f3addf76b
EgonOlsen <info@jpct.net>
2021-11-26 10:42:10 +0100

    Fixed optimizer flaw with setting to 0

--- src/main/java/com/sixtyfour/cbmnative/mos6502/Optimizer6502.java
+++ src/main/java/com/sixtyfour/cbmnative/mos6502/Optimizer6502.java
@@ -537,7 +537,7 @@ public class Optimizer6502 implements Optimizer {
 		return new ArrayList<Pattern>() {
 			private static final long serialVersionUID = 1L;
 			{
-				this.add(new Pattern("Simplified setting to 0", new String[] { "LDA #0", "STA {MEM0}", "STA {MEM0}+1" },
+				this.add(new Pattern("Simplified setting to 0", new String[] { "LDA #0", "STA {MEM0}", "STA {MEM0}+1", "STA {MEM0}+2", "STA {MEM0}+3", "STA {MEM0}+4" },
 						"LDA #<{#0.0}", "LDY #>{#0.0}", "JSR REALFAC", "LDX #<{MEM0}", "LDY #>{MEM0}", "JSR FACMEM"));
 				this.add(new Pattern(false, "Faster logic OR", new String[] { "JSR FASTOR" }, "JSR FACOR"));
 				this.add(new Pattern(false, "Faster logic AND", new String[] { "JSR FASTAND" }, "JSR ARGAND"));
