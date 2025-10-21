e0e94ba78c1671c84ed88ed51b357da783f23bad
EgonOlsen <github@jpct.net>
2025-10-21 20:51:52 +0200

    Fix for https://github.com/EgonOlsen71/basicv2/issues/68

--- dist/mospeed.sh
+++ dist/mospeed.sh
@@ -1,1 +1,9 @@
-java -Xmx768m -cp basicv2.jar:dist/basicv2.jar com.sixtyfour.cbmnative.shell.MoSpeedCL $@
+#!/bin/bash
+# Resolve the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Running from $SCRIPT_DIR"
+
+# Run Java using classpath relative to the script's directory
+java -Xmx768m -cp "$SCRIPT_DIR/basicv2.jar:$SCRIPT_DIR/dist/basicv2.jar" com.sixtyfour.cbmnative.shell.MoSpeedCL "$@"
+
