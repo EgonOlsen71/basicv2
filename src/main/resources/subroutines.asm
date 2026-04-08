0495caa135864da4d819e157f76d7ae27ce09bcd
Foerster-H <helge.foerster@schluetersche.de>
2026-04-08 14:27:13 +0200

    Made usage of SEC/CLD save

--- src/main/resources/subroutines.asm
+++ src/main/resources/subroutines.asm
@@ -1498,13 +1498,14 @@ CONVPOSINT
 			RTS
 ;###################################
 INT2BCD
-			SED
 			LDA #0
 			STA BCD
 			STA BCD+1
 			STA BCD+2
 			ASL TMP_ZP
 			ROL TMP_ZP+1
+			SEI
+			SED
 			LDA BCD
 			ADC BCD
 			STA BCD
@@ -1556,6 +1557,7 @@ BCDBIT2
 			DEX
 			BNE BCDBIT2
 			CLD
+			CLI
 			RTS
 BCD
 			.WORD 0 0
